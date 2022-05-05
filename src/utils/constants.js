e0f008ef9709c39ec0e306c069b1c850f7ecb097
Vinutana <vinutanavm@gmail.com>
2022-05-05 21:44:28 +0530

    country-weather-app

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const COUNTRY_API = "https://restcountries.com/v3.1/name";
+
+const ACCESS_KEY = "de8c75672de440fe3f001b3bc1b129ab";
+
+export const WEATHER_API = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}`;
